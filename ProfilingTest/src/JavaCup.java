45db45f7a96e075dbf3aa10b8501eb30b1bcd484
arezekhanari <a.reza.khanari@gmail.com>
2024-12-01 18:11:10 +0330

    Optimize temp function

--- ProfilingTest/src/JavaCup.java
+++ ProfilingTest/src/JavaCup.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JavaCup {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Press number1: ");
         int i = scanner.nextInt();
@@ -14,20 +13,22 @@ public class JavaCup {
         temp();
         eval(i, j, k);
     }
-    public static void eval(int i, int j, int k)
-    {
-        if (i * i + j * j == k * k || i * i == j * j + k * k || j * j == i * i + k * k)
-        {
+
+    public static void eval(int i, int j, int k) {
+        if (i * i + j * j == k * k || i * i == j * j + k * k || j * j == i * i + k * k) {
             System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
-        else { System.out.println("NO"); }
     }
+
     public static void temp() {
-        ArrayList a = new ArrayList();
-        for (int i = 0; i < 10000; i++)
-        {
+        int[] a = new int[10000 * 20000];
+        int cpunter = 0;
+        for (int i = 0; i < 10000; i++) {
             for (int j = 0; j < 20000; j++) {
-                a.add(i + j);
+                a[cpunter] = i + j;
+                cpunter += 1;
             }
         }
     }
